3653dcf64848623c3b39da78ce9aeef9bbd8f93d
cristian980829 <cristian980829@gmail.com>
2021-09-20 03:03:09 -0500

    lasta details

--- frontend/src/app/components/private-tasks/private-tasks.component.ts
+++ frontend/src/app/components/private-tasks/private-tasks.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { TasksService } from 'src/app/services/tasks.service';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-private-tasks',
@@ -11,15 +12,18 @@ import { Router } from '@angular/router';
 export class PrivateTasksComponent implements OnInit {
 
   privateTasks: any = [];
-  constructor(private taskService: TasksService, private router: Router) { }
+  constructor(private taskService: TasksService, private router: Router,
+    private authService: AuthService) { }
 
   ngOnInit() {
     this.taskService.getPrivateTasks()
       .subscribe(
-        res => this.privateTasks = res,
+        res => {
+          this.privateTasks = res},
         err => {
           if (err instanceof HttpErrorResponse) {
             if (err.status === 401) {
+              this.authService.logout();
               this.router.navigate(['/signin']);
             }
           }
